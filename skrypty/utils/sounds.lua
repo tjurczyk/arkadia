0efc832e8c9bf474016eccff9e2cf9a7aa0405e8
Piotr Wilczynski <delwing@gmail.com>
2020-04-17 01:06:46 +0200

    - sound directory correction

--- skrypty/utils/sounds.lua
+++ skrypty/utils/sounds.lua
@@ -1,5 +1,5 @@
 function scripts_play_beep()
-    playSoundFile(getMudletHomeDir() .. [[/sounds/beep.wav]])
+    playSoundFile(getMudletHomeDir() .. [[/arkadia/sounds/beep.wav]])
 end
 
 registerAnonymousEventHandler("playBeep", "scripts_play_beep")

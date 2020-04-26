297d94a31fd7d231f3e6b5c97ff7ea2afdb640c0
Piotr Wilczynski <piotr.wilczynski@bisnode.com>
2020-04-26 19:57:13 +0200

    - add after death progress highlight

--- skrypty/character.lua
+++ skrypty/character.lua
@@ -1,7 +1,16 @@
--------------------------------------------------
--- Put your Lua functions here.        --
--- --
--- Note that you can also use external Scripts --
--------------------------------------------------
+scripts.character.stats = scripts.character.stats or {}
 
+function scripts.character.stats:after_death_progress()
+    if selectString(matches[1], 1) > -1 then
+        fg("SpringGreen")
+    end
 
+    if selectString(matches[2], 1) > -1 then
+        fg("DeepSkyBlue")
+    end
+    progress_replace(matches[2])
+end
+
+function trigger_func_after_death_progress()
+    scripts.character.stats:after_death_progress()
+end

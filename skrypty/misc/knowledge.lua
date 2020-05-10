f4173407ac8be4f218098d3b5927cef4f896df2c
Mariusz Jamro <mariusz.jamro@gmail.com>
2020-05-10 23:03:44 +0200

    Ułatwienie komend do czytania w bibliotekach (wiedza)

--- skrypty/misc/knowledge.lua
+++ skrypty/misc/knowledge.lua
@@ -19,3 +19,41 @@ function misc:knowledge_replace(text)
     resetFormat()
 end
 
+last_knowledge = nil
+
+-- rozbija output dostepnych do zglebiania wiedzy na linie
+function misc:zglebiaj_replace(text)
+    selectString(text, 1)
+    replace("\n")
+
+    text = string.gsub(text, "czy o ([^?]*)?", ", o %1")
+    local available = string.split(text, ",")
+    last_knowledge = available
+    for i,var in pairs(available) do
+        var = string.lower(string.trim(var))
+        cecho(''..i..'. '..var..'\n')
+    end
+end
+
+function misc:zglebiaj_wiedze(index)
+    index = tonumber(index)
+    if last_knowledge == nil then
+        send('zglebiaj')
+        return
+    end
+
+    local name = last_knowledge[index]
+    if name == nil then
+        cecho("<orange>Nie odnalazlem wiedzy o tym indeksie. Sprobuj podac pelna nazwe.\n")
+        return
+    end
+    send('zglebiaj wiedze '..name)
+end
+
+function alias_func_skrypty_misc_zglebiaj_wiedze()
+    misc:zglebiaj_wiedze(tonumber(matches[2]))
+end
+
+function trigger_func_skrypty_misc_knowledge_O_czym_wiedze_checesz_zglebiac()
+    misc:zglebiaj_replace(matches[2])
+end

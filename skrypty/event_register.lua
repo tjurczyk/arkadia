4524511822242ec061e42b766cdcc8695cf8c607
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2020-05-12 11:19:13 +0200

    poprawa silnika event handler

--- skrypty/event_register.lua
+++ skrypty/event_register.lua
@@ -6,12 +6,16 @@ scripts.event_register = scripts.event_register or {
     Register event handler and store its id
 
     Args:
+    event: event's name
+    callback: callable function
+    onetime: if one time
     Just as in https://wiki.mudlet.org/w/Manual:Miscellaneous_Functions#registerAnonymousEventHandler
 --]]
 function scripts.event_register:register_event_handler(event, callback, onetime)
-    local id = registerAnonymousEventHandler(event, callback, onetime)
-    table.insert(scripts.event_register.handlers, id)
-    return id
+    local mudlet_id = registerAnonymousEventHandler(event, callback, onetime)
+    local internal_id = #scripts.event_register.handlers + 1
+    scripts.event_register.handlers[internal_id] = mudlet_id
+    return internal_id
 end
 
 --[[
@@ -19,28 +23,27 @@ end
     Otherwise return that reference
 
     Args:
-    reference: value to be checked as condition to register handler
+    internal_id: internal handler's id to be registered as singleton.
     Just as in https://wiki.mudlet.org/w/Manual:Miscellaneous_Functions#registerAnonymousEventHandler
 --]]
-function scripts.event_register:register_singleton_event_handler(reference, event, callback, onetime)
-   if reference ~= nil then
-       return reference
-   else
+function scripts.event_register:register_singleton_event_handler(internal_id, event, callback, onetime)
+    if scripts.event_register.handlers[internal_id] ~= nil then
+        return internal_id
+    else
         return scripts.event_register:register_event_handler(event, callback, onetime)
    end
 end
 
 --[[
-    Register event handler and store its id, kill handler for reference passed
-    Otherwise return that reference
+    Force kill handler with internal_id, then register a new handler and return a new handler id.
 
     Args:
-    reference: value to be checked as condition to register handler
+    internal_id: internal handler's id to be killed.
     Just as in https://wiki.mudlet.org/w/Manual:Miscellaneous_Functions#registerAnonymousEventHandler
 --]]
-function scripts.event_register:force_register_event_handler(reference, event, callback, onetime)
-    if reference ~= nil then
-        pcall(killAnonymousEventHandler, reference)
+function scripts.event_register:force_register_event_handler(internal_id, event, callback, onetime)
+    if scripts.event_register.handlers[internal_id] ~= nil then
+        pcall(killAnonymousEventHandler, scripts.event_register.handlers[internal_id])
     end
     return scripts.event_register:register_event_handler(event, callback, onetime)
 end
@@ -51,10 +54,10 @@ end
     Args:
     id - id under which event was registered
 --]]
-function scripts.event_register:kill_event_handler(id)
-    if scripts.event_register.handlers[id] then
-        killAnonymousEventHandler(scripts.event_register.handlers[id])
-        scripts.event_register[id] = nil
+function scripts.event_register:kill_event_handler(internal_id)
+    if scripts.event_register.handlers[internal_id] then
+        killAnonymousEventHandler(scripts.event_register.handlers[internal_id])
+        scripts.event_register[internal_id] = nil
     end
 end
 
@@ -62,8 +65,8 @@ end
     Clear event register and kill all handlers
 --]]
 function scripts.event_register:kill_all_event_handlers()
-    for k, v in pairs(scripts.event_register.handlers) do
-        killAnonymousEventHandler(v)
+    for _, mudlet_id in pairs(scripts.event_register.handlers) do
+        killAnonymousEventHandler(mudlet_id)
     end
-    scripts.scripts.event_register.handlers = {}
+    scripts.event_register.handlers = {}
 end

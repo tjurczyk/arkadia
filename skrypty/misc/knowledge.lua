010f6c170108c92e8327381d8ca2ed4b9451b059
Piotr <delwing@gmail.com>
2020-05-15 11:55:08 +0200

    knowledge rework (#546)

--- skrypty/misc/knowledge.lua
+++ skrypty/misc/knowledge.lua
@@ -11,7 +11,9 @@ misc["knowledge_desc"] = {
     ["pelna"] = "      [10/10]==========|",
 }
 
-function misc:knowledge_replace(text)
+misc.knowledge = misc.knowledge or {}
+
+function misc.knowledge:knowledge_replace(text)
     selectString(text, 1)
     local add_text = " " .. misc.knowledge_desc[text]
     replace(text .. add_text)
@@ -19,50 +21,48 @@ function misc:knowledge_replace(text)
     resetFormat()
 end
 
-last_knowledge = nil
+function misc.knowledge:initial_zglebiaj()
+    self.trigger = tempRegexTrigger("Wiedze o czym chcesz zglebiac\\? (.*)", function() self:zglebiaj_replace(matches[2]) end, 1)
+    send("zglebiaj")
+    tempTimer(5, function() disableTrigger(self.trigger) end)
+end
 
 -- rozbija output dostepnych do zglebiania wiedzy na linie
-function misc:zglebiaj_replace(text)
+function misc.knowledge:zglebiaj_replace(text)
     selectString(text, 1)
     replace("\n")
 
     text = string.gsub(text, "czy o ([^?]*)?", ", o %1")
     local available = string.split(text, ",")
-    last_knowledge = available
+    self.last_knowledge = available
     for i,var in pairs(available) do
         var = string.lower(string.trim(var))
         cecho(''..i..'. '..var..'\n')
     end
 
-    disableTrigger("zglebiaj")
+    killTrigger(self.trigger)
 end
 
-function misc:zglebiaj_wiedze(index)
+function misc.knowledge:zglebiaj_wiedze(index)
     index = tonumber(index)
-    if last_knowledge == nil then
+    if self.last_knowledge == nil then
         send('zglebiaj')
         return
     end
 
-    local name = last_knowledge[index]
+    local name = self.last_knowledge[index]
     if name == nil then
         cecho("<orange>Nie odnalazlem wiedzy o tym indeksie. Sprobuj podac pelna nazwe.\n")
         return
     end
-    send('zglebiaj wiedze '..name)
+    send("zglebiaj wiedze "..name)
 end
 
 function alias_func_skrypty_misc_zglebiaj_wiedze()
     local number = string.trim(matches[2])
     if (number == nil or number == '') then
-        enableTrigger("zglebiaj")
-        send('zglebiaj')
-        tempTimer(1, [[ disableTrigger("zglebiaj") ]])
+        misc.knowledge:initial_zglebiaj()
     else
-        misc:zglebiaj_wiedze(tonumber(number))
+        misc.knowledge:zglebiaj_wiedze(tonumber(number))
     end  
-end
-
-function trigger_func_skrypty_misc_knowledge_O_czym_wiedze_checesz_zglebiac()
-    misc:zglebiaj_replace(matches[2])
 end

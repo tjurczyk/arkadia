a678d73447076d19cf9582fcac332ba21787f385
Piotr Wilczynski <delwing@gmail.com>
2020-05-15 10:36:02 +0200

    add combat state determinig

---
+++ skrypty/character/combat_state.lua
@@ -0,0 +1,53 @@
+scripts.character.combat_state = scripts.character.combat_state or {
+    handlers = {},
+    state = false,
+}
+
+function scripts.character.combat_state:init()
+    for _, event_id in pairs(self.handlers) do
+        scripts.event_register:kill_event_handler(event_id)
+    end
+
+    table.insert(self.handlers, scripts.event_register:register_event_handler("ateam_am_attacked", function(_, state) if state then self:start_combat() else self:end_combat() end end))
+end
+
+function scripts.character.combat_state:start_combat()
+    self.state = true
+    if self.timer then
+        killTimer(self.timer)
+    end
+end
+
+function scripts.character.combat_state:end_combat()
+    if self.state then
+        self.time_after_combat = 30
+        self.timer = tempTimer(1, function() self:update() end, true)
+        self.state = false
+        if self.trigger then
+            killTrigger(self.trigger)
+        end
+        self.trigger = tempExactMatchTrigger("Ochlon troche od walki...", function()
+            creplaceLine(matches[1] .. " (" .. self:get_cooloff_timer() .. "s)")
+            self.command = command
+        end)
+    end
+end
+
+function scripts.character.combat_state:get_cooloff_timer()
+    return self.time_after_combat
+end
+
+function scripts.character.combat_state:update()
+    self.time_after_combat = self.time_after_combat - 1
+    if self.time_after_combat == 0 then
+        raiseEvent("cooledAfterCombat")
+        killTimer(self.timer)
+        killTrigger(self.trigger)
+        if self.command then
+            scripts.utils.bind_functional(command)
+        end
+        self.command = false
+    end
+end
+
+scripts.character.combat_state:init()

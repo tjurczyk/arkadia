94d30b29a426cde0fbae37c862fdb9050ebadd77
Piotr Wilczynski <delwing@gmail.com>
2020-05-15 11:24:52 +0200

    make installer store version from tag and use it to check newest version

--- skrypty/utils/installer/latest.lua
+++ skrypty/utils/installer/latest.lua
@@ -35,7 +35,8 @@ function scripts.latest:compare(version, false_callback, true_callback)
         return true_callback()
     end
 
-    local current_version_partials_list = split(scripts.ver, "\.")
+    local current_version_partials_list = split(scripts.installer:get_version_from_file() or scripts.ver, "\.")
+    display(current_version_partials_list)
     local repository_version_partial_list = split(version, "\.")
     for i = 1, math.max(table.size(current_version), table.size(repository_version_partial_list))  do
             if (current_version_partials_list[i] or "0") > (repository_version_partial_list[i] or "0") then

2f7797ac5c5ebfbb4960fce3f3ab33e3a8ce7cfd
Piotr Wilczynski <delwing@gmail.com>
2020-05-30 01:45:44 +0200

    - remove plugin from scripts.plugin on removal

--- skrypty/utils/installer/plugins.lua
+++ skrypty/utils/installer/plugins.lua
@@ -1,6 +1,6 @@
 scripts.plugins_installer = scripts.plugins_installer or {
     plugin_directory = getMudletHomeDir() .. "/plugins/",
-    valid_extensions = {"zip", "mpackage"}
+    valid_extensions = { "zip", "mpackage" }
 }
 
 function scripts.plugins_installer:install_from_url(url)
@@ -31,7 +31,9 @@ function scripts.plugins_installer:install_from_url(url)
         scripts:print_log("[WARN] Rozszerzenie moze byc nieprawidlowe. Oczekiwane rozszerzenia to .zip, .mpackage lub brak")
     end
 
-    self.plugin_download_handler = scripts.event_register:force_register_event_handler("download_plugin", "sysDownloadDone", function(_, filename) self:handle_download(_, filename, plugin_name, branch) end)
+    self.plugin_download_handler = scripts.event_register:force_register_event_handler("download_plugin", "sysDownloadDone", function(_, filename)
+        self:handle_download(_, filename, plugin_name, branch)
+    end)
     self.plugin_zip = self.plugin_directory .. file_name
     downloadFile(self.plugin_zip, url)
     scripts:print_log("Pobieram plugin " .. plugin_name .. "(" .. url .. ")")
@@ -43,8 +45,12 @@ function scripts.plugins_installer:handle_download(_, filename, plugin_name, bra
     end
 
     scripts.event_register:kill_event_handler(self.plugin_download_handler)
-    scripts.event_register:register_event_handler("sysUnzipDone", function(event, ...) self:handle_unzip(event, plugin_name, branch,  ...) end, true)
-    scripts.event_register:register_event_handler("sysUnzipError", function(event, ...) self:handle_unzip(event, plugin_name, branch,  ...) end, true)
+    scripts.event_register:register_event_handler("sysUnzipDone", function(event, ...)
+        self:handle_unzip(event, plugin_name, branch, ...)
+    end, true)
+    scripts.event_register:register_event_handler("sysUnzipError", function(event, ...)
+        self:handle_unzip(event, plugin_name, branch, ...)
+    end, true)
     local directory = branch and "" or plugin_name
     unzipAsync(self.plugin_zip, self.plugin_directory .. directory)
 end
@@ -69,6 +75,12 @@ function scripts.plugins_installer:uninstall(plugin_name)
         return
     end
     uninstallPackage(plugin_name)
+    for k, plugin_name_to_check in pairs(scripts.plugins) do
+        if plugin_name_to_check == plugin_name then
+            table.remove(scripts.plugins, k)
+            break
+        end
+    end
     scripts.installer.delete_dir(self.plugin_directory .. plugin_name)
     scripts:print_log("Plugin " .. plugin_name .. " odinstalowany")
 end

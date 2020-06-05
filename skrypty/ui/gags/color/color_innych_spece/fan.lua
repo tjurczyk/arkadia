c18bb8119ba35f6e5e0b7c721ec22305f8a6d95e
Piotr <delwing@gmail.com>
2020-06-05 18:25:17 +0200

    fan spec correction (#606)

--- skrypty/ui/gags/color/color_innych_spece/fan.lua
+++ skrypty/ui/gags/color/color_innych_spece/fan.lua
@@ -1,12 +1,3 @@
-function trigger_func_skrypty_ui_gags_color_color_innych_spece_fan_ktos_spec_7()
-    selectCurrentLine()
-    local str_replace = "[FAN 7/7] "
-    prefix(str_replace)
-    selectString(str_replace, 1)
-    fg(scripts.gag_colors["innych_spece"])
-    resetFormat()
-end
-
 function trigger_func_skrypty_ui_gags_color_color_innych_spece_fan_ktos_spec_0()
     selectCurrentLine()
     local str_replace = "[FAN 0/7] "

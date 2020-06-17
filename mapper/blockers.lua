2c5a6b6ab156ab11319260135e8cdcf62922ba40
Piotr <delwing@gmail.com>
2020-06-17 10:08:06 +0200

    Blocker improvement (#610)
    * blockers improvement
    
    * blocker corrections
    
    * - correct regexp
    
    * - remove not needed code
    
    * add event description
    correct camp entrance special exit add
    
    * correct exti
    
    * typo fix
    
    * send walking event for special exits as well
    
    * apply universal block to almost (all blockers)
    
    Co-authored-by: Piotr Wilczynski <piotr.wilczynski@bisnode.com>

--- mapper/blockers.lua
+++ mapper/blockers.lua
@@ -1,44 +1,30 @@
-function trigger_func_mapper_blockers_blockers()
+amap.blockers = amap.blockers or {
+    is_blockable = false
+}
+
+function amap.blockers:init()
+    scripts.event_register:register_singleton_event_handler("blocker1", "amapWalking", function() self:set_blockable(true) end)
+    scripts.event_register:register_singleton_event_handler("blocker2", "gmcp.room.info", function() self:set_blockable(false) end)
+end
+
+function amap.blockers:block()
     if amap.mode == "off" then
         return
     end
 
+    self:set_blockable(false)
     raiseEvent("amapBlockerFired")
     amap:move_backward()
     amap:terminate_walker()
 end
 
-function trigger_func_mapper_blockers_blockers2()
-    if amap.mode == "off" then
-        return
-    end
-
-    raiseEvent("amapBlockerFired")
-    amap:move_backward()
-    amap:terminate_walker()
-end
-
-function trigger_func_mapper_blockers_blockers3()
-    if amap.mode == "off" then
-        return
-    end
-
-    raiseEvent("amapBlockerFired")
-    amap:move_backward()
-    amap:terminate_walker()
+function amap.blockers:set_blockable(state)
+    self.is_blockable = state
 end
 
 function trigger_func_mapper_blockers_blocker_team_dependent()
-    -- they only work if no team or team and I'm the leader
-
-    if amap.mode == "off" then
-        return
-    end
-
-    if ateam.objs[ateam.my_id]["team_leader"] or not ateam.objs[ateam.my_id]["team"] then
-        raiseEvent("amapBlockerFired")
-        amap:move_backward()
-        amap:terminate_walker()
+    if amap.blockers.is_blockable then
+        amap.blockers:block()
     end
 end
 
@@ -56,3 +42,8 @@ function trigger_func_mapper_blockers_blockers_block()
     amap:terminate_walker()
 end
 
+function trigger_func_blockers_unblockable()
+    amap.blockers:set_blockable(false)
+end
+
+amap.blockers:init()

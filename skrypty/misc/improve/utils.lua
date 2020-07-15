79d790c607c0ed72932fcb1d88702454ace1fa73
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2020-07-14 21:42:08 -0700

    rewizja do obliczania czasu i do msg (#630)
    Co-authored-by: Tomasz Jurczyk <tomasz@moveworks.ai>

--- skrypty/misc/improve/utils.lua
+++ skrypty/misc/improve/utils.lua
@@ -1,11 +1,22 @@
 function misc.improve:seconds_to_formatted_string(seconds)
     local minutes = math.floor(seconds / 60)
-    local minutes_string
+    local minutes_string = ""
+
+    if minutes > 60 then
+        hours = math.floor(minutes / 60)
+        if hours < 10 then
+            minutes_string = "0" .. tostring(hours)
+        else
+            minutes_string = tostring(hours)
+        end
+        minutes_string = minutes_string .. ":"
+        minutes = minutes - (hours * 60)
+    end
 
     if minutes < 10 then
-        minutes_string = "0" .. tostring(minutes)
+        minutes_string = minutes_string .. "0" .. tostring(minutes)
     else
-        minutes_string = tostring(minutes)
+        minutes_string = minutes_string .. tostring(minutes)
     end
 
     local seconds = math.floor(seconds % 60)

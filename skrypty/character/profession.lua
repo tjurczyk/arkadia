7446d9dbbafcbf12e1133d75c9a7d8feb15b10a7
Piotr Wilczynski <delwing@gmail.com>
2020-09-20 23:45:50 +0200

    fix situations when new week was added on sundays

--- skrypty/character/profession.lua
+++ skrypty/character/profession.lua
@@ -33,7 +33,7 @@ end
 
 function scripts.character.profession:get_next_break_point(time)
     local date = os.date("*t", time)
-    local date_diff = date.wday <= 2 and 2 - date.wday or 7 - date.wday
+    local date_diff = (7 - date.wday) % 7
     date.day = date.day + date_diff
     date.hour = 2
     date.min = 0

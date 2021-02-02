2e62f1a8450a267739ef1ab059146b46b2fbf80d
Marcin Załuski <mzaluski@opera.com>
2021-02-02 21:01:58 +0100

    dodano ikony i kolorowe paski stanu do states_window aktywowane przez scripts.ui.states_fancy

--- skrypty/team.lua
+++ skrypty/team.lua
@@ -37,10 +37,70 @@ ateam = ateam or {
     }
 }
 
-states = { [6] = "<green>#######", [5] = "<green>-######", [4] = "<yellow>--#####", [3] = "<yellow>---####", [2] = "<red>----###", [1] = "<red>-----##", [0] = "<red>------#" }
-states_enemy_no_color = { [6] = "#######", [5] = "-######", [4] = "--#####", [3] = "---####", [2] = "----###", [1] = "-----##", [0] = "------#" }
-states_normal = states
-states_enemy = states
+if scripts.ui.states_fancy then
+
+	scripts.ui.object_icons = {
+		["mezczyzna"] = "🧑🏻",
+		["kobieta"] = "👩🏻‍🦰",
+		["straznik"] = "💂",
+		["elf"] = "🧝",
+		["elfka"] = "🧝‍",
+		["drwal"] = "🧔🏻",
+		["ogr"] = "👹",
+		["ogrzyca"] = "👹",
+		["krasnolud"] = "🎅🏻",
+		["krasnoludka"] = "🎅🏻",
+		["gnom"] = "👨🏽‍🦰",
+		["gnomka"] = "👩🏽‍🦰",
+		["niziolek"] = "👨🏻‍🍳",
+		["halfling"] ="👨🏻‍🍳",
+		["niziolka"] ="👨🏻‍🍳",
+		["halflinka"] ="👨🏻‍🍳",
+		["wilk"] ="🐺",
+		["zajac"]="🐰",
+		["krolik"]="🐰",
+		["lis"]="🦊",
+		["mysz"]="🐭",
+		["szczur"]="",
+		["krowa"]="🐮",
+		["swinia"]="🐷",
+		["niedzwiedz"]="🐻",
+		["smok"]="🐲",
+		["pies"]="🐕",
+		["waz"]="🐍",
+		["nietoperz"]="🦇",
+		["kogut"]="🐔",
+		["kura"]="",
+		["kon"]="🐴",
+		["wiewiorka"]="🐿",
+		["kot"]="🐱",
+		["jaszczurka"]="🦎",
+		["jaszczur"]="🦎",
+		["kaczka"]="🦆",
+		["owca"]="🐑",
+		["koza"]="🐐",
+		["baran"]="🐏",
+		["dzik"]="🐗",
+		["zombie"]="🧟",
+		["ghoul"]="🧟",
+		["szkielet"]="💀",
+		["kosciotrup"]="💀",
+		["goblin"]="👺",
+		}
+
+	states = { [6] = "<green>▇▇▇▇▇▇▇😎<reset>", [5] = "<dark_green>▇<green>▇▇▇▇▇▇🙂<reset>", [4] = "<ansi_yellow>▇▇<yellow>▇▇▇▇▇😐<reset>", [3] = "<saddle_brown>▇▇▇<orange>▇▇▇▇😕<reset>", [2] = "<ansi_red>▇▇▇▇<red>▇▇▇😞<reset>", [1] = "<ansi_red>▇▇▇▇▇<deep_pink>▇▇😫<reset>", [0] = "<ansi_red>▇▇▇▇▇▇<deep_pink>▇💀<reset>" }
+
+	states_normal = { [6] = "<ansi_cyan>▇▇▇▇▇▇▇👤<reset>", [5] = "<dark_slate_grey>▇<ansi_cyan>▇▇▇▇▇▇👤<reset>", [4] = "<dark_slate_grey>▇▇<ansi_cyan>▇▇▇▇▇👤<reset>", [3] = "<dark_slate_grey>▇▇▇<ansi_cyan>▇▇▇▇👤<reset>", [2] = "<dark_slate_grey>▇▇▇▇<ansi_cyan>▇▇▇👤<reset>", [1] = "<dark_slate_grey>▇▇▇▇▇<cyan>▇▇👤<reset>", [0] = "<dark_slate_grey>▇▇▇▇▇▇<cyan>▇💀<reset>" }
+	
+	states_enemy = { [6] = "<firebrick>▇▇▇▇▇▇▇😈<reset>", [5] = "<ansi_red>▇<firebrick>▇▇▇▇▇▇😈<reset>", [4] = "<ansi_red>▇▇<firebrick>▇▇▇▇▇👿<reset>", [3] = "<ansi_red>▇▇▇<firebrick>▇▇▇▇👿<reset>", [2] = "<ansi_red>▇▇▇▇<red>▇▇▇😡<reset>", [1] = "<ansi_red>▇▇▇▇▇<red>▇▇🥵<reset>", [0] = "<ansi_red>▇▇▇▇▇▇<deep_pink>▇💀<reset>" }
+	
+	states_enemy_no_color = { [6] = "▇▇▇▇▇▇▇😈", [5] = "▇▇▇▇▇▇▇😈", [4] = "▇▇▇▇▇▇▇👿", [3] = "▇▇▇▇▇▇▇👿", [2] = "▇▇▇▇▇▇▇😡", [1] = "▇▇▇▇▇▇▇🥵", [0] = "▇▇▇▇▇▇▇💀" }
+else
+	states = { [6] = "<green>#######", [5] = "<green>-######", [4] = "<yellow>--#####", [3] = "<yellow>---####", [2] = "<red>----###", [1] = "<red>-----##", [0] = "<red>------#" }
+	states_enemy_no_color = { [6] = "#######", [5] = "-######", [4] = "--#####", [3] = "---####", [2] = "----###", [1] = "-----##", [0] = "------#" }
+	states_normal = states
+	states_enemy = states
+end
 
 ateam["footer_info_attack_mode_to_text"] = { "A", "AW", "AWR" }
 

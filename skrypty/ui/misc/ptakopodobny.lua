a0dbf0a3fa8b4f88d18a11c0a1419d20ff5bfb0b
Mariusz Jamro <mariusz.jamro@gmail.com>
2021-02-03 09:50:48 +0100

    Update skrypty/ui/misc/ptakopodobny.lua
    Co-authored-by: Piotr <delwing@gmail.com>

--- skrypty/ui/misc/ptakopodobny.lua
+++ skrypty/ui/misc/ptakopodobny.lua
@@ -1,9 +1,3 @@
 function trigger_func_skrypty_ui_misc_ptakopodobny_inkantuje()
-    cecho("\n\n")
-    cecho("<red>+---------------------------------------------+\n")
-    cecho("<red>|                                             |\n")
-    cecho("<red>|          KAPLAN CZARUJE - UCIEKAJ!!!        |\n")
-    cecho("<red>|                                             |\n")
-    cecho("<red>+---------------------------------------------+\n")
-    cecho("\n\n")
+    scripts.messages:warning("KAPŁAN CZARUJE - UCIEKAJ!!!")
 end    

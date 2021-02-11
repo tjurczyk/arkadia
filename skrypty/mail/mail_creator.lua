030077b50c98af62a714b990cae21b80a36ad75e
Piotr Wilczynski <delwing@gmail.com>
2021-02-12 00:26:28 +0100

    bugfix + justowanie

--- skrypty/mail/mail_creator.lua
+++ skrypty/mail/mail_creator.lua
@@ -10,7 +10,8 @@ scripts.mail_creator = scripts.mail_creator or {
         plain_border = require("skrypty.mail.plain_border_template"),
         parchment = require("skrypty.mail.parchment_mail_template")
     },
-    template = "plain"
+    template = "plain",
+    justify = true
 }
 
 scripts.mail_creator.width = 55
@@ -42,6 +43,9 @@ end
 
 local function justify(line, limit)
     limit = limit or scripts.mail_creator.width
+    if line:len() < limit / 2 then
+        return line
+    end
     local words = string.split(line, " ")
     local n_words = table.size(words) * 2 - 1
     local space = limit - line:len()
@@ -51,8 +55,10 @@ local function justify(line, limit)
     end
     while space > 0 do
       space = space - 1
-      words[current_index] = words[current_index] .. " "
-      current_index = current_index + 2
+      if words[current_index] then
+        words[current_index] = words[current_index] .. " "
+        current_index = current_index + 2
+      end
       if current_index > n_words then
         current_index = 2
       end
@@ -155,8 +161,10 @@ function mailCreatorAddLine(line)
     end
 
     local lines = wrap(line)
-    for k, line in pairs(lines) do
-        lines[k] = justify(line)
+    if self.justify then
+        for k, line in pairs(lines) do
+            lines[k] = justify(line)
+        end
     end
     for _, wrapped in pairs(lines) do
         scripts.mail_creator:append_input(wrapped)

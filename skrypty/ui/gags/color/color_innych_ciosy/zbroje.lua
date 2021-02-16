a3293f1aff80ef126127b797655652497cf8f16f
Mariusz Jamro <mariusz.jamro@gmail.com>
2021-02-16 20:28:11 +0100

    Rozdzielilem spece na moje/innych. Dodalem ciemnogranatowy helm

--- skrypty/ui/gags/color/color_innych_ciosy/zbroje.lua
+++
@@ -1,9 +0,0 @@
-function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_zbroje_baron_tarcza()
-    scripts.gags:gag_prefix("HERB SPEC", "innych_ciosy")
-end
-
-function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_zbroje_lsniaca_tarcza()
-    scripts.gags:gag_prefix("LSN SPEC", "innych_ciosy")
-
-end
-

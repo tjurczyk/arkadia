15e06e976b64451e812fbdf2d270ab86aef74577
Mariusz Jamro <mariusz.jamro@gmail.com>
2021-03-01 09:32:36 +0100

    Spec halabardy i poprawki na kilka innych wzorców.

--- skrypty/ui/gags/color/color_moje_spece/sprzet.lua
+++ skrypty/ui/gags/color/color_moje_spece/sprzet.lua
@@ -1,14 +1,16 @@
 
 function trigger_func_skrypty_ui_gags_color_color_moje_spece_zbroje_blekitno_srebrna_trojkatna_tarcza()
-    scripts.gags:gag_prefix("HERB SPEC", "moje_spece")
+    scripts.gags:gag_prefix("TARCZA SPEC", "moje_spece")
 end
 
 function trigger_func_skrypty_ui_gags_color_color_moje_spece_zbroje_lsniaca_plomienista_tarcza()
-    scripts.gags:gag_prefix("LSN SPEC", "moje_spece")
+    scripts.gags:gag_prefix("TARCZA SPEC", "moje_spece")
 end
 
 function trigger_func_skrypty_ui_gags_color_color_moje_spece_zbroje_ciemnogranatowy_smukly_helm()
     scripts.gags:gag_prefix("HELM SPEC", "moje_spece")
 end
 
-
+function trigger_func_skrypty_ui_gags_color_color_moje_spece_bronie_ciernista_kobaltowa_halabarda()
+    scripts.gags:gag_prefix("BRON SPEC", "moje_spece")
+end

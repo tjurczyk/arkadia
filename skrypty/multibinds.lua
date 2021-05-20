e22004f4a611fd246c20837af3fdb303d9fa0362
Piotr Wilczynski <delwing@gmail.com>
2021-05-20 10:24:05 +0200

    tworzenie nastepnego binda

--- skrypty/multibinds.lua
+++ skrypty/multibinds.lua
@@ -1,4 +1,6 @@
-scripts.multibinds = scripts.multibinds or {}
+scripts.multibinds = scripts.multibinds or {
+    max_binds = 4
+}
 
 scripts.multibinds.db = db:create("multibinds", {
     multibinds = {
@@ -19,6 +21,10 @@ function scripts.multibinds:init()
 end
 
 function scripts.multibinds:create(room_id, index, action)
+    if index > self.max_binds then
+        scripts:print_log(string.format("Numer binda musi byc pomiedzy 1, a %d", self.max_binds))
+        return
+    end
     db:add(self.db.multibinds, {
         room_id = room_id,
         index = index,
@@ -26,6 +32,7 @@ function scripts.multibinds:create(room_id, index, action)
         uniqness = string.format("%s-%s", room_id, index)
     })
     scripts:print_log(string.format("Utworzono bind %s-%s: %s", room_id, index, action))
+    self:update_ui(amap.curr.id)
 end
 
 function scripts.multibinds:create_current(index, action)
@@ -33,12 +40,31 @@ function scripts.multibinds:create_current(index, action)
     self:update_ui(amap.curr.id)
 end
 
-function scripts.multibinds:clear_location(room_id)
-    db:delete(self.db.multibinds, db:eq(self.db.multibinds.room_id, room_id))
+function scripts.multibinds:create_next(action)
+    local binds = self:get_for_location(amap.curr.id)
+    local slots = {}
+    for i = 1, self.max_binds, 1 do
+        table.insert(slots, i)
+    end
+    for k,v in pairs(binds) do
+        table.remove(slots, table.index_of(slots, v.index))
+    end
+    display(slots)
+    if slots[1] then
+        self:create(amap.curr.id, slots[1], action)
+    else
+        scripts:print_log(string.format("Lokacja ma juz maksymalna (%d) liczbe bindow.", self.max_binds))
+    end
 end
 
-function scripts.multibinds:clear_location(room_id, index)
-    db:delete(self.db.multibinds, { db:eq(self.db.multibinds.room_id, room_id), db:eq(self.db.multibinds.index, index) })
+function scripts.multibinds:clear_location(room_id)
+    db:delete(self.db.multibinds, db:eq(self.db.multibinds.room_id, room_id))
+    self:update_ui(amap.curr.id)
+end
+
+function scripts.multibinds:clear_location_index(room_id, index)
+    db:delete(self.db.multibinds, db:AND(db:eq(self.db.multibinds.room_id, room_id), db:eq(self.db.multibinds.index, index)))
+    self:update_ui(amap.curr.id)
 end
 
 function scripts.multibinds:get_for_location(room_id)
@@ -89,22 +115,26 @@ function scripts.multibinds:run_current(index)
 
  scripts.multibinds:init()
 
- function alias_func_mbind_create_current(index, action)
+function alias_func_mbind_create_current(index, action)
     scripts.multibinds:create_current(index, action)
  end
 
- function alias_func_mbind_display_current()
+ function alias_func_mbind_create_next(action)
+    scripts.multibinds:create_next(action)
+ end
+
+function alias_func_mbind_display_current()
     scripts.multibinds:display(amap.curr.id)
  end
 
- function alias_func_mbind_display(room_id)
+function alias_func_mbind_display(room_id)
     scripts.multibinds:display(room_id)
  end
 
- function alias_func_mbind_clear_location()
+function alias_func_mbind_clear_location()
      scripts.multibinds:clear_location(amap.curr.id)
  end
 
- function alias_func_mbind_clear_location_index(index)
+function alias_func_mbind_clear_location_index(index)
     scripts.multibinds:clear_location_index(amap.curr.id, index)
 end

9778f5da202a01c872672d375786862b6d886d9e
Rawisz <r.wiszowaty@gmail.com>
2021-06-10 16:34:46 +0200

    Spec tarczy w oknie kondycji

--- skrypty/ui/gags/color/color_innych_spece/sprzet.lua
+++ skrypty/ui/gags/color/color_innych_spece/sprzet.lua
@@ -1,6 +1,7 @@
 
 function trigger_func_skrypty_ui_gags_color_color_innych_spece_zbroje_blekitno_srebrna_trojkatna_tarcza()
     scripts.gags:gag_prefix("TARCZA SPEC", "innych_spece")
+    ateam:may_setup_paralyzed_name(matches[2])
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_spece_zbroje_lsniaca_plomienista_tarcza()

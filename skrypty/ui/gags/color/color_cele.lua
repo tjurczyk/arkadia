79f6fa3bc1ed707adaa7e2752892a87196a5d19c
Rawisz <r.wiszowaty@gmail.com>
2021-06-11 20:25:00 +0200

    RA wskazanie celu ataku

--- skrypty/ui/gags/color/color_cele.lua
+++ skrypty/ui/gags/color/color_cele.lua
@@ -16,12 +16,8 @@ function trigger_func_skrypty_ui_gags_color_color_cele_cel_ataku_ktos()
         return
     end
     
-    if matches[3] == "ciebie" then
-        local uppered = string.upper(matches[3])
-        creplaceLine("<salmon>\n\n[  CEL ATT   ] " .. matches[2] .. uppered .. " jako CEL ATAKU.\n\n")
-    else
-        creplaceLine("<salmon>\n\n[  CEL ATT   ] " .. matches[2] .. matches[3] .. " jako CEL ATAKU.\n\n")
-    end
+    creplaceLine("<salmon>\n\n[  CEL ATT   ] " .. matches[2] .. " CEL ATAKU.\n\n")
+
     resetFormat()
 end
 

475f5638983815ded7acfbad4bc1cc9888cb67dc
rawisz <58864031+rawisz@users.noreply.github.com>
2021-06-11 08:16:21 +0200

    Ogluch barba i granita w oknie kondycji (#985)
    * Ogluch barba i granita w oknie kondycji
    
    * Spec tarczy w oknie kondycji

--- skrypty/ui/gags/color/color_innych_spece/bar.lua
+++ skrypty/ui/gags/color/color_innych_spece/bar.lua
@@ -1,5 +1,6 @@
 function trigger_func_skrypty_ui_gags_color_color_innych_spece_bar_ktos_granit()
     scripts.gags:gag_prefix("GRA OGL", "innych_spece")
+    ateam:may_setup_paralyzed_name(matches[2])
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_spece_bar_ktos_fin()

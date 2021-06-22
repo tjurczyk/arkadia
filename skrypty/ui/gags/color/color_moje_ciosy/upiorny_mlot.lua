e433a8140b76281011cbeafa85aa764d6440f619
rawisz <58864031+rawisz@users.noreply.github.com>
2021-06-22 10:48:54 +0200

    Gryfi mlot, upiorny mlot (#1015)
    * Gryfi mlot - gag sila ciosu
    
    * upiorny mlot

---
+++ skrypty/ui/gags/color/color_moje_ciosy/upiorny_mlot.lua
@@ -0,0 +1,19 @@
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_upiorny_mlot_1()
+    scripts.gags:gag(1, 4, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_upiorny_mlot_2()
+    scripts.gags:gag(2, 4, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_upiorny_mlot_3()
+    scripts.gags:gag(3, 4, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_upiorny_mlot_4()
+    scripts.gags:gag(4, 4, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_upiorny_mlot_fin()
+    scripts.gags:gag_prefix("FIN", "moje_ciosy")
+end

a0ca9b6b46597fbe32375b28450e47b3fa0d6c12
rawisz <58864031+rawisz@users.noreply.github.com>
2021-06-23 17:29:51 +0200

    Drobne zmiany (#1022)

--- skrypty/misc/food.lua
+++ skrypty/misc/food.lua
@@ -2,3 +2,6 @@ function trigger_func_skrypty_misc_food_siadanie()
     scripts.utils.bind_functional("usiadz " .. string.lower(matches[3]))
 end
 
+function trigger_func_skrypty_misc_food_usiadz()
+    scripts.utils.bind_functional("usiadz")
+end

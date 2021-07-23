b0f6d72153007d15e7aa86d3b32a12df15964352
Piotr Wilczynski <delwing@gmail.com>
2021-07-23 11:52:04 +0200

    alrekiny 1os

--- skrypty/ui/gags/color/magics.lua
+++ skrypty/ui/gags/color/magics.lua
@@ -1,3 +1,16 @@
+-- Misterny
+
+function trigger_func_skrypty_ui_gags_moje_ciosy_misterny(value)
+    scripts.gags:gag(value, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_misterny_innych(value)
+    local target = rex.match(getCurrentLine(), "\\b(?:ciebie|cie|ci)\\b") and "innych_ciosy_we_mnie" or "innych_ciosy"
+    scripts.gags:gag(value, 6, target)
+end
+
+-- Opal
+
 function trigger_func_skrypty_ui_gags_moje_ciosy_opal_spec(value)
     scripts.gags:gag(value, 5, "moje_ciosy")
 end
@@ -15,4 +28,9 @@ end
 function trigger_func_skrypty_ui_gags_opal_spec_innych(value)
     local target = rex.match(getCurrentLine(), "\\b(?:ciebie|cie|ci)\\b") and "innych_ciosy_we_mnie" or "innych_ciosy"
     scripts.gags:gag(value, 5, target)
+end
+
+-- Arlekiny
+function trigger_func_skrypty_ui_gags_moje_ciosy_arlekiny(value)
+    scripts.gags:gag(value, 6, "moje_ciosy")
 end

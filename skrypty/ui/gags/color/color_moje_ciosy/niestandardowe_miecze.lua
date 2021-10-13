d42ce7d163a4ede67e005110818d74946ce0d7c5
rawisz <58864031+rawisz@users.noreply.github.com>
2021-10-13 20:28:46 +0200

    Bronie opisy (#1136)
    * wulkaninczy mlot
    
    * Niestandardowe ciosy broni

---
+++ skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_miecze.lua
@@ -0,0 +1,56 @@
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_ledwo_muskasz()
+    scripts.gags:gag(1, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_lekko_ranisz()
+    scripts.gags:gag(2, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_ranisz()
+    if matches[2] == "powaznie" or matches[2] == "ciezko" then return end
+    scripts.gags:gag(3, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_powaznie_ranisz()
+    scripts.gags:gag(4, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_bardzo_ciezko_ranisz()
+    scripts.gags:gag(5, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_jatagan_masakrujesz()
+    scripts.gags:gag(6, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_1()
+    scripts.gags:gag(1, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_2()
+    scripts.gags:gag(2, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_3()
+    scripts.gags:gag(3, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_4()
+    scripts.gags:gag(4, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_5()
+    scripts.gags:gag(5, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_6()
+    scripts.gags:gag(6, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_7()
+    scripts.gags:gag(7, 7, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_espadon_fin()
+    scripts.gags:gag_prefix("FIN", "moje_ciosy")
+end

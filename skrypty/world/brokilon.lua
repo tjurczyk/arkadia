6ee764d0f74d3928763eafa36a5e162e5744529f
Piotr Wilczynski <delwing@gmail.com>
2021-10-26 20:42:06 +0200

    dodanie rusalki

--- skrypty/world/brokilon.lua
+++ skrypty/world/brokilon.lua
@@ -12,3 +12,20 @@ function trigger_func_strzaly()
     fg("orange_red")
     resetFormat()
 end
+
+function trigger_func_rusalka()
+    prefix("[ UROK ]  ")
+    selectCurrentLine()
+    fg("orange_red")
+    resetFormat()
+    scripts.messages:show("UROK RUSALKI", "orange_red")
+    scripts.utils.bind_functional_call("/zz rusalke",  "/zz rusalke")
+end
+
+function trigger_func_rusalka2()
+    prefix("[ UROK ]  ")
+    selectCurrentLine()
+    fg("orange_red")
+    resetFormat()
+    scripts.utils.bind_functional_call("/zz rusalke",  "/zz rusalke")
+end

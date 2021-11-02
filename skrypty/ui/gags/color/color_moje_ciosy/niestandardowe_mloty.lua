70342b142d1b72d0ea23fccab4611d4f039bf9eb
rawisz <58864031+rawisz@users.noreply.github.com>
2021-11-02 17:32:52 +0100

    dwusegmentowa maczuga (#1159)
    * Niestandardowe bronie
    
    * Sugestie i wiecej exact match
    
    * wielki runiczny mlot wojenny

--- skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_mloty.lua
+++ skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_mloty.lua
@@ -121,3 +121,31 @@ end
 function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_gryfi_mlot_fin()
     scripts.gags:gag_prefix("FIN", "moje_ciosy")
 end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_1()
+    scripts.gags:gag(1, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_2()
+    scripts.gags:gag(2, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_3()
+    scripts.gags:gag(3, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_4()
+    scripts.gags:gag(4, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_5()
+    scripts.gags:gag(5, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_6()
+    scripts.gags:gag(6, 6, "moje_ciosy")
+end
+
+function trigger_func_skrypty_ui_gags_color_color_moje_ciosy_runiczny_mlot_fin()
+    scripts.gags:gag_prefix("FIN", "moje_ciosy")
+end

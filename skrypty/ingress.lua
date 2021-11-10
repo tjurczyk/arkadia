ad39bc825ab028b57528eb9cc2b4f00c50f7003d
Piotr Wilczynski <delwing@gmail.com>
2021-11-10 15:54:50 +0100

    clear prompt

--- skrypty/ingress.lua
+++ skrypty/ingress.lua
@@ -3,6 +3,8 @@ scripts.ingress = scripts.ingress or {
     handler = function() end
 }
 
+local prompt_sequence = "> "
+
 function scripts.ingress:init()
     self.options_handler = scripts.event_register:force_register_event_handler(self.options_handler, "gmcp.char.options", function() self:set_prompt() end)
     self.msg_handler = scripts.event_register:force_register_event_handler(self.msg_handler, "gmcp.gmcp_msgs", function()
@@ -12,7 +14,10 @@ function scripts.ingress:init()
 end
 
 function scripts.ingress:set_prompt()
-    if (gmcp.char.options.info and gmcp.char.options.info.no_prompt) then
+    if gmcp.char.options and gmcp.char.options.no_prompt ~= nil then
+        self.prompt = gmcp.char.options.no_prompt == 0
+    end
+    if gmcp.char.options.info and gmcp.char.options.info.no_prompt then
         -- wymagane, poniewaz arkadia nieprawidlowo wysyla no_prompt
         tempRegexTrigger("^ZNAK zachety:\\s+(Wlaczony|Wylaczony)", function()
             self.prompt = matches[2] == "Wlaczony"
@@ -38,13 +43,13 @@ end
 function scripts.ingress:prompt_handler()
     local msg = dec(gmcp.gmcp_msgs.text)
     local plain = ansi2string(msg)
-    if plain:sub(1,1) == ">" and plain:len() > 2 then
-        local find = plain:sub(2,2) ~= " " and ">" or " "
-        local separator_point = msg:find(find)
-        feedTriggers(msg:sub(separator_point + 1))
-    else
-        feedTriggers(msg)
+    local feed_msg = msg
+    while plain:sub(1,2) == prompt_sequence and plain:len() > 2 do
+        local separator_point = msg:find(prompt_sequence)
+        feed_msg = feed_msg:sub(separator_point + 2)
+        plain = ansi2string(feed_msg)
     end
+    feedTriggers(feed_msg)
     return msg
 end
 

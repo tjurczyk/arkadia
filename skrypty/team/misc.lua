67612012029455720daba65db6ac4ff1c6349fed
Mariusz Jamro <mariusz.jamro@gmail.com>
2021-11-13 23:35:37 +0100

    Komenda /ostatnio

--- skrypty/team/misc.lua
+++ skrypty/team/misc.lua
@@ -23,6 +23,53 @@ function ateam:check_team_here()
     end
 end
 
+function ateam:check_team_last_activity()
+
+    self.triggerId = self.triggerId or nil;
+    if self.triggerId then
+        killTrigger(self.triggerId)
+    end
+
+    local members = {}
+    local max_length = 0
+    
+    for k, v in pairs(ateam.team) do
+        if type(v) == "number" and v ~= ateam.my_id and table_has_value(gmcp.objects.nums, v) == true then
+            local desc = ateam.objs[v]["desc"]
+            table.insert(members, desc)
+            if string.len(desc) > max_length then
+                max_length = string.len(desc)
+            end
+        end
+    end
+
+    if table.size(members) == 0 then
+        scripts:print_log("Nie jestes w druzynie")
+        return
+    end
+
+    self.triggerId = tempTrigger("Aktywnosc", function() 
+        selectCurrentLine()
+        local line = getCurrentLine()
+
+        local color = 'ansi_light_red'
+        if string.find(line, ': aktywn') then
+            color = 'green'
+        end
+        fg(color)
+        resetFormat()
+
+        selectString("Aktywnosc          :", 1)
+        local replacement = "Aktywnosc "..scripts.utils.str_pad(members[1]..":", max_length + 1)
+        replace(replacement)
+        table.remove(members,1)
+    end, table.size(members))
+
+    for k, v in pairs(members) do
+        send('ostatnio ' .. v, false)
+    end
+end
+
 function ateam:can_perform_sneaky_attack()
     local hidden_count_number = tonumber(scripts.ui.states_window_nav_states.hidden_state)
     if not hidden_count_number then

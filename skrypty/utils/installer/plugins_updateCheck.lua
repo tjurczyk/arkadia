4a0b982cf247f6373fcd9050f8838ceae2baa5ab
Paweł Karczmarczuk <axesider@gmail.com>
2022-01-31 14:57:14 +0100

    generyczny plugins update Check

---
+++ skrypty/utils/installer/plugins_updateCheck.lua
@@ -0,0 +1,39 @@
+scripts.plugins_updateCheck = scripts.plugins_updateCheck or {
+    plugins = {}
+}
+
+function scripts.plugins_updateCheck:getFile(config)
+    return getMudletHomeDir() .. "/plugins/" .. config.plugin_name .. "/commits"
+end
+
+function scripts.plugins_updateCheck:checkNewVersion(plugin_name, repo)
+    local config = {["plugin_name"] = plugin_name, ["repo"] = repo, ["storeKey"] = repo .. plugin_name}
+    config.url = "https://api.github.com/repos/" .. config.repo .. "/" .. config.plugin_name .. "/commits"
+    config.handlerId = registerAnonymousEventHandler("sysGetHttpDone", function(event, url, response) return self:onHttpGetDone(url, response) end)
+    table.insert(self.plugins, config)
+    getHTTP(config.url)
+end
+
+function scripts.plugins_updateCheck:onHttpGetDone(url, response)
+    for i = 1, #self.plugins do
+        local config = self.plugins[i]
+        if config.url == url then
+            local contents = yajl.to_value(response)
+            local sha = contents[1].sha
+            local State = scripts.state_store:get(config.storeKey) or {}
+            if State.sha ~= nil and sha ~= State.sha then
+                cecho("\n<CadetBlue>(skrypty)<tomato>: Plugin '<light_slate_blue>".. config.plugin_name .. "<tomato>' posiada nowa aktualizacje. Kliknij ")
+                cechoLink("<green>tutaj", function() scripts.plugins_updateCheck:update(config, sha) end, "Aktualizuj", true)
+                cecho("<tomato> aby ja pobrac.\n")
+            end
+            killAnonymousEventHandler(config.handlerId)
+        end
+    end
+end
+
+function scripts.plugins_updateCheck:update(config, update_sha)
+    scripts.plugins_installer:install_from_url("https://codeload.github.com/" .. config.repo .. "/" .. config.plugin_name .. "/zip/master")
+    local State = scripts.state_store:get(config.storeKey) or {}
+    State.sha = update_sha
+    scripts.state_store:set(config.storeKey, State)
+end

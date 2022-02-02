9ef0446eb7447f1ec2aa52b4762c45a5ef04a2ce
Paweł Karczmarczuk <axesider@gmail.com>
2022-02-02 13:37:15 +0100

    snake_case

--- skrypty/utils/installer/plugins_updateCheck.lua
+++ skrypty/utils/installer/plugins_updateCheck.lua
@@ -1,20 +1,16 @@
-scripts.plugins_updateCheck = scripts.plugins_updateCheck or {
+scripts.plugins_update_check = scripts.plugins_update_check or {
     plugins = {}
 }
 
-function scripts.plugins_updateCheck:getFile(config)
-    return getMudletHomeDir() .. "/plugins/" .. config.plugin_name .. "/commits"
-end
-
-function scripts.plugins_updateCheck:checkNewVersion(plugin_name, repo)
-    local config = {["plugin_name"] = plugin_name, ["repo"] = repo, ["storeKey"] = repo .. plugin_name}
+function scripts.plugins_update_check:github_check_version(plugin_name, repo)
+    local config = {["plugin_name"] = plugin_name, ["repo"] = repo, ["storeKey"] = 'github_' .. repo .."_" .. plugin_name}
     config.url = "https://api.github.com/repos/" .. config.repo .. "/" .. config.plugin_name .. "/commits"
-    config.handlerId = registerAnonymousEventHandler("sysGetHttpDone", function(event, url, response) return self:onHttpGetDone(url, response) end)
+    config.handlerId = registerAnonymousEventHandler("sysGetHttpDone", function(event, url, response) return self:github_on_GetHttpDone(url, response) end)
     table.insert(self.plugins, config)
     getHTTP(config.url)
 end
 
-function scripts.plugins_updateCheck:onHttpGetDone(url, response)
+function scripts.plugins_update_check:github_on_GetHttpDone(url, response)
     for i = 1, #self.plugins do
         local config = self.plugins[i]
         if config.url == url then
@@ -23,15 +19,16 @@ function scripts.plugins_updateCheck:onHttpGetDone(url, response)
             local State = scripts.state_store:get(config.storeKey) or {}
             if State.sha ~= nil and sha ~= State.sha then
                 cecho("\n<CadetBlue>(skrypty)<tomato>: Plugin '<light_slate_blue>".. config.plugin_name .. "<tomato>' posiada nowa aktualizacje. Kliknij ")
-                cechoLink("<green>tutaj", function() scripts.plugins_updateCheck:update(config, sha) end, "Aktualizuj", true)
+                cechoLink("<green>tutaj", function() scripts.plugins_update_check:github_update(config, sha) end, "Aktualizuj", true)
                 cecho("<tomato> aby ja pobrac.\n")
             end
             killAnonymousEventHandler(config.handlerId)
+            config.handlerId = nil
         end
     end
 end
 
-function scripts.plugins_updateCheck:update(config, update_sha)
+function scripts.plugins_update_check:github_update(config, update_sha)
     scripts.plugins_installer:install_from_url("https://codeload.github.com/" .. config.repo .. "/" .. config.plugin_name .. "/zip/master")
     local State = scripts.state_store:get(config.storeKey) or {}
     State.sha = update_sha

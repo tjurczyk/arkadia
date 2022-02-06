8925e92931e0bb3ecd9550067891b5588f2314e0
eldakar <eldakar@pop-os.localdomain>
2022-02-06 00:23:50 +0000

    fix

--- skrypty/team/core.lua
+++ skrypty/team/core.lua
@@ -348,7 +348,8 @@ function ateam:print_obj_team(id, obj)
             local val_countenemies = table.size(ateam.team_enemies[id])
             if val_countenemies > 0 then
                 str_attackers = " " .. "<red:team_console_bg>" .. val_countenemies
-            elseif val_countenemies > 9 then
+            end
+            if val_countenemies > 9 then
                 str_attackers = "<red:team_console_bg>" .. val_countenemies
             end
             cecho(scripts.ui.states_window_name, "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_left.."<reset>" .. str_attackers .. "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_right)
@@ -482,7 +483,8 @@ function ateam:print_obj_enemy(id, obj)
             local val_countenemies = table.size(ateam.attacking_by_team[id])
             if val_countenemies > 0 then
                 str_attackers = " " .. "<red:team_console_bg>" .. val_countenemies
-            elseif val_countenemies > 9 then
+            end
+            if val_countenemies > 9 then
                 str_attackers = "<red:team_console_bg>" .. val_countenemies
             end
             cecho(scripts.ui.enemy_states_window_name, "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_left.."<reset>" .. str_attackers .. "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_right)
@@ -601,7 +603,8 @@ function ateam:print_obj_normal(id, obj)
             local val_countenemies = table.size(ateam.attacking_by_team[id])
             if val_countenemies > 0 then
                 str_attackers = " " .. "<red:team_console_bg>" .. val_countenemies
-            elseif val_countenemies > 9 then
+            end
+            if val_countenemies > 9 then
                 str_attackers = "<red:team_console_bg>" .. val_countenemies
             end
             cecho(scripts.ui.enemy_states_window_name, "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_left.."<reset>" .. str_attackers .. "<"..ateam.options.bracket_hp_color..":team_console_bg>"..ateam.options.bracket_symbol_right)

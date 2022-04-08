954ca1958f00e6a911afaaaf4fa67e1c4ad336f7
Marcin Szydelski <marcin@szydelscy.pl>
2022-04-08 23:38:09 +0200

    dodajemy follow do room_id, a nie do current

--- mapper/db/core.lua
+++ mapper/db/core.lua
@@ -139,7 +139,7 @@ function amap.db:add_follow_link(room_id, from_link, to_link)
         error("Wrong input")
     end
 
-    local curr_data = getRoomUserData(amap.curr.id, "team_follow_link")
+    local curr_data = getRoomUserData(room_to_set, "team_follow_link")
     if curr_data ~= "" then
         local new_data = curr_data .. "#" .. from_link .. "*" .. to_link
         setRoomUserData(room_to_set, "team_follow_link", new_data)

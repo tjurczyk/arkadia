79952f0a53e1d65dd2209cf3edb893153d5a773f
Paweł Karczmarczuk <axesider@gmail.com>
2022-05-06 14:47:31 +0200

    Lepsze states
    # Conflicts:
    #	skrypty.lua
    #	skrypty/ui/footer_elements.lua
    #	skrypty/ui/states_window/ui_states_window_navbar.lua

--- skrypty/ui/states_window/ui_states_window_navbar.lua
+++ skrypty/ui/states_window/ui_states_window_navbar.lua
@@ -3,23 +3,25 @@ function scripts.ui:navbar_updates(name, force)
         return
     end
 
-    scripts.ui.states_window_navbar_str = ""
+    local navbar_str = ""
 
     for k, v in pairs(scripts.ui.cfg["states_window_nav_elements"]) do
         local key = scripts.ui:decode_states_window_navbar_key(v)
         local printable_key = scripts.ui.cfg.states_window_nav_printable_key_map[v]
         local printable_val = scripts.ui:navbar_get_printable_elem(key)
-
+       
         if printable_val == nil then
-            printable_val = scripts.ui.states_window_nav_states[name]
+            printable_val = scripts.ui.states_window_nav_states[key]
         end
-
-        scripts.ui.states_window_navbar_str = scripts.ui.states_window_navbar_str .. printable_key .. "<white>: " .. printable_val
+        
+        navbar_str = navbar_str .. printable_key .. "<white>: " .. printable_val
 
         if k ~= #scripts.ui.cfg["states_window_nav_elements"] then
-            scripts.ui.states_window_navbar_str = scripts.ui.states_window_navbar_str .. "<white> | "
+            navbar_str = navbar_str .. "<white> | "
         end
     end
+
+    scripts.ui.states_window_navbar_str = navbar_str
 
     ateam:print_status()
 end

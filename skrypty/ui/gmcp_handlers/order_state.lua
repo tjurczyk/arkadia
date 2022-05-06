2e6a49dd179f8a376f3f4c24906cd5a0d7106349
axesider <axesider@gmail.com>
2022-05-06 21:16:58 +0200

    Better UI states (#1335)
    * Lepsze states
    
    # Conflicts:
    #	skrypty.lua
    #	skrypty/ui/footer_elements.lua
    #	skrypty/ui/states_window/ui_states_window_navbar.lua
    
    * regexp fix
    naming

--- skrypty/ui/gmcp_handlers/order_state.lua
+++ skrypty/ui/gmcp_handlers/order_state.lua
@@ -1,11 +1,27 @@
 function order_state(name, seconds)
-    scripts.ui:info_order_ready_update(tostring(seconds))
-    scripts.ui:navbar_updates(name)
+end
+
+function timer_func_skrypty_order_timer()
+    local dt = getEpoch() - scripts.ui.order_state_epoch
+    if dt >= 15 then
+        stopNamedTimer("arkadia", "order_timer")
+        scripts.ui.states_window_nav_states["order_state"] = scripts.ui["states_window_nav_printable_val_map"]["order_state"]["ok"]
+        scripts.ui:info_order_ready_update("ok")
+    else
+        local val = string.format("%.1f", dt)
+        scripts.ui.states_window_nav_states["order_state"] = "<red>" .. val
+        scripts.ui:info_order_ready_update(val)
+    end
+    scripts.ui:navbar_updates("order_state")
+    raiseEvent("order_state", dt)
 end
 
 if scripts.event_handlers["skrypty/ui/gmcp_handlers/order_state.order_state.order_state"] then
     killAnonymousEventHandler(scripts.event_handlers["skrypty/ui/gmcp_handlers/order_state.order_state.order_state"])
 end
 
+scripts.ui.order_state_epoch = 0
 scripts.event_handlers["skrypty/ui/gmcp_handlers/order_state.order_state.order_state"] = registerAnonymousEventHandler("order_state", order_state)
 
+registerNamedTimer("arkadia", "order_timer", 0.1, timer_func_skrypty_order_timer, true)
+stopNamedTimer("arkadia", "order_timer")

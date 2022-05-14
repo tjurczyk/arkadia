8c6ce13f9b58b68336aec8b2a8a34feeb63bd42d
Paweł Karczmarczuk <axesider@gmail.com>
2022-05-14 09:14:21 +0200

    Config do odliczania

--- skrypty/ui/gmcp_handlers/guard_state.lua
+++ skrypty/ui/gmcp_handlers/guard_state.lua
@@ -2,13 +2,14 @@ function guard_state(name, seconds)
 end
 
 function timer_func_skrypty_cover_timer()
+    local limit = 5
     local dt = getEpoch() - scripts.ui.guard_state_epoch
-    if dt >= 5 then
+    if dt >= limit then
         stopNamedTimer("arkadia", "cover_timer")
         scripts.ui.states_window_nav_states["guard_state"] = false
         scripts.ui:info_cover_ready_update("ok")
     else
-        local val = string.format("%.1f", dt)
+        local val = string.format("%.1f", ateam.options.countdown and (limit - dt) or dt)
         scripts.ui.states_window_nav_states["guard_state"] = "<red>" .. val
         scripts.ui:info_cover_ready_update(val)
     end

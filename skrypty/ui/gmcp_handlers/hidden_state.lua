dd176005437217e88037fdbb4c7c6fd9d8c269ac
Piotr Wilczynski <delwing@gmail.com>
2022-05-18 16:47:39 +0200

    Revert "Revert "Better UI states (#1335)""
    This reverts commit 24b19a760f1d9627622f74c255ac9856ca10bd52.

--- skrypty/ui/gmcp_handlers/hidden_state.lua
+++ skrypty/ui/gmcp_handlers/hidden_state.lua
@@ -1,11 +1,28 @@
 function hidden_state(name, seconds)
-    scripts.ui:info_hidden_update(seconds)
-    scripts.ui:navbar_updates(name)
+end
+
+function timer_func_skrypty_hidden_timer()
+    local dt = getEpoch() - scripts.ui.hidden_state_epoch
+    if dt >= 15 then
+        stopNamedTimer("arkadia", "hidden_timer")
+        scripts.ui.states_window_nav_states["hidden_state"] = ""
+        scripts.ui:info_hidden_update("")
+    else
+        local val = string.format("%i", dt)
+        scripts.ui.states_window_nav_states["hidden_state"] = "<red>" .. val
+        scripts.ui:info_hidden_update(val)
+    end
+    ateam:print_status()
+    scripts.ui:navbar_updates("hidden_state")
+    raiseEvent("hidden_state", dt)
 end
 
 if scripts.event_handlers["skrypty/ui/gmcp_handlers/hidden_state.hidden_state.hidden_state"] then
     killAnonymousEventHandler(scripts.event_handlers["skrypty/ui/gmcp_handlers/hidden_state.hidden_state.hidden_state"])
 end
 
+scripts.ui.hidden_state_epoch = 0
 scripts.event_handlers["skrypty/ui/gmcp_handlers/hidden_state.hidden_state.hidden_state"] = registerAnonymousEventHandler("hidden_state", hidden_state)
 
+registerNamedTimer("arkadia", "hidden_timer", 0.1, timer_func_skrypty_hidden_timer, true)
+stopNamedTimer("arkadia", "hidden_timer")

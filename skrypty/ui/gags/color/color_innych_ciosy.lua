0bef656c59d658452724a7a579d637f760b44380
rawisz <58864031+rawisz@users.noreply.github.com>
2022-05-23 02:29:59 +0200

    Standardowe ciosy bez regexpa (#1349)
    * standardowe ciosy bez regexpa
    
    * Poprawki
    
    * Poprawki v2

--- skrypty/ui/gags/color/color_innych_ciosy.lua
+++ skrypty/ui/gags/color/color_innych_ciosy.lua
@@ -1,13 +1,20 @@
-local function color_hit(val)
-    if line:starts("W slepiach") then
-        return
+local function color_hit(value)
+    local target
+    if line:match(" cie ") then
+        target = "innych_ciosy_we_mnie"
+
+        selectString(matches[1] .. "cie", 1)
+        setFgColor(255, 153, 51)
+        resetFormat()
+    else
+        target = "innych_ciosy"
     end
-    scripts.gags:gag(val, 6, "innych_ciosy")
+    scripts.gags:gag(value, 6, target)
 end
 
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_ledwo_muska()
-    if string.match(matches[2], "blyskawicznie") then
+    if rex.match(line, "upiorn\\w+ ciemn\\w+") then
         return
     end
 
@@ -15,43 +22,42 @@ function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_ledwo_muska(
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_lekko_rani()
-    if string.match(matches[2], "blyskawicznie") then
-        return
-    end
+    local ignore_list = {
+        "srebrzystej kosy",
+        "opalizujacego runicznego",
+        "adamantytowego mlota"
+    }
 
-    if rex.match(line, "adamantytow\\w+ mlot\\w+ bojow\\w+") then
-        return
-    end
-
-    if string.match(line, "srebrzystej kosy bojowej") then
-        return
+    for _, v in pairs(ignore_list) do
+        if line:match(v) then
+            return
+        end
     end
 
     color_hit(2)
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_rani()
-    local ignore_list = {
-        ["lekko"] = true,
-        ["powaznie"] = true,
-        ["ciezko"] = true,
-        ["paskudnie"] = true,
-        ["i"] = true,
-        ["mocno"] = true,
-        ["krwawo"] = true,
-        ["smiertelnie"] = true,
-        ["espadona"] = true
-    }
+        local ignore_list = {
+            "lekko",
+            "powaznie",
+            "ciezko",
+            " i ",
+            "mocno",
+            "smiertelnie"
+        }
 
-    if ignore_list[matches[3]] then
+        for _, v in pairs(ignore_list) do
+            if line:match(v) then
+                return
+            end
+        end
+
+    if line:starts("Dostrzegajac szanse na skuteczny atak") then
         return
     end
 
-    if string.match(matches[2], "blyskawicznie") then
-        return
-    end
-
-    if string.starts(matches[1], "Dostrzegajac szanse na skuteczny atak") then
+    if line:starts("W slepiach") then
         return
     end
 
@@ -64,17 +70,11 @@ end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_powaznie_rani()
     local ignore_list = {
-        ["blyskawicznie"] = true,
-        ["jatagana"] = true,
-        ["poteznym"] = true,
-        ["pelnego"] = true,
-        ["paskudnie"] = true,
-        ["Twardym"] = true,
-        ["desperacki"] = true
+        "poteznym ciosem"
     }
 
-    for k, v in pairs(ignore_list) do
-        if string.match(matches[2], k) then
+    for _, v in pairs(ignore_list) do
+        if line:match(v) then
             return
         end
     end
@@ -83,46 +83,20 @@ function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_powaznie_ran
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_bardzo_ciezko_rani()
-    local ignore_list = {
-        ["blyskawicznie"] = true,
-        ["jatagana"] = true,
-        ["poteznym"] = true,
-        ["pelnego"] = true,
-        ["paskudnie"] = true,
-        ["Twardym"] = true,
-        ["desperacki"] = true
-    }
-
-    for k, v in pairs(ignore_list) do
-        if string.match(matches[2], k) then
-            return
-        end
-    end
-
     color_hit(5)
 end
 
 function trigger_func_skrypty_ui_gags_color_color_innych_ciosy_ktos_masakruje()
-    local ignore_list = {
-        ["blyskawicznie"] = true,
-        ["jatagana"] = true,
-        ["poteznym"] = true,
-        ["pelnego"] = true,
-        ["paskudnie"] = true,
-        ["Twardym"] = true,
-        ["desperacki"] = true,
-        ["espadonem"] = true
-    }
+        local ignore_list = {
+            " i ",
+            "srebrzysta kosa bojowa"
+        }
 
-    for k, v in pairs(ignore_list) do
-        if string.match(matches[2], k) then
-            return
+        for _, v in pairs(ignore_list) do
+            if line:match(v) then
+                return
+            end
         end
-    end
-
-    if string.match(line, "srebrzysta kosa bojowa") then
-        return
-    end
 
     if line:starts("Ostrze poszczerbionego oburecznego miecza") then
         return
@@ -133,10 +107,10 @@ end
 
 function trigger_func_skrypty_ui_gags_innych_ciosy_pajeczaki(value)
 
-    if value == 3 and string.match(line, "powaznie") then
+    if value == 3 and line:match("powaznie") then
         return
     end
 
-    local target = string.match(line, " cie ") and "innych_ciosy_we_mnie" or "innych_ciosy"
+    local target = line:match(" cie ") and "innych_ciosy_we_mnie" or "innych_ciosy"
     scripts.gags:gag(value, 6, target)
 end
--- skrypty/ui/misc_fighting.lua
+++ skrypty/ui/misc_fighting.lua
@@ -22,52 +22,6 @@ function trigger_func_skrypty_ui_misc_fighting_uciekl_ci()
     resetFormat()
 end
 
-function trigger_func_skrypty_ui_misc_fighting_ledwo_lekko()
-    selectString(matches[3], 1)
-    setFgColor(45, 185, 45)
-    resetFormat()
-end
-
-function trigger_func_skrypty_ui_misc_fighting_ranisz_powaznie()
-    selectString(matches[3], 1)
-    setFgColor(45, 185, 45)
-    resetFormat()
-end
-
-function trigger_func_skrypty_ui_misc_fighting_bciezko_masakrujesz()
-    selectString(matches[3], 1)
-    setFgColor(45, 185, 45)
-    resetFormat()
-end
-
-function trigger_func_skrypty_ui_misc_fighting_ledwo_lekko_cie()
-    selectString(matches[4], 1)
-    setFgColor(255, 153, 51)
-    resetFormat()
-end
-
-function trigger_func_skrypty_ui_misc_fighting_rani_powaga_cie()
-    if matches[4] == "lekko" or matches[4] == "smiertelnie" then
-        return
-    end
-
-    if matches[4] == "powaznie" then
-        selectString(matches[4], 1)
-        setFgColor(255, 153, 51)
-        resetFormat()
-    end
-
-    selectString(matches[5], 1)
-    setFgColor(255, 153, 51)
-    resetFormat()
-end
-
-function trigger_func_skrypty_ui_misc_fighting_bciezko_masakruje_ciebie()
-    selectString(matches[4], 1)
-    setFgColor(255, 153, 51)
-    resetFormat()
-end
-
 function trigger_func_skrypty_ui_misc_fighting_wspiera()
     if matches[3] == "cie" then
         return

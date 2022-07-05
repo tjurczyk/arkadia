f0168a9f0e3a0abec3e670b3c5a4d533b9135e7b
rawisz <58864031+rawisz@users.noreply.github.com>
2022-07-05 22:29:17 +0200

    Fix cpm, adamantytowy mlot - podwojne wartosci (#1386)
    * widoczne kierunki na drodze czaszek
    
    * follow - pulapka kolo saurgla
    
    * Salignac elfka - strzalki
    
    * Blocker issue #1385
    
    * Rozdzielony follow dla studni
    
    * Fix cpm, adamantytowy mlot - podwojne wartosci

--- skrypty/ui/gags/color/color_moje_ciosy.lua
+++ skrypty/ui/gags/color/color_moje_ciosy.lua
@@ -1,6 +1,13 @@
 function trigger_func_skrypty_ui_gags_color_moje_ciosy(value)
-    if line:match("opalizujacego runicznego")  then
-        return
+    local ignore_list = {
+        "opalizujacego runicznego",
+        "czarnoblekitnego pulsujacego morgensterna"
+    }
+
+    for _, v in pairs(ignore_list) do
+        if line:match(v) then
+            return
+        end
     end
 
     if rex.match(line, "srebrzyst\\w+ kos\\w+ bojow\\w+") then

a1f912aa41b6d79f5bb5c5a52cfcbeaaefb39302
Piotr Wilczynski <delwing@gmail.com>
2022-10-13 18:49:59 +0200

    fix update check

--- skrypty/utils/installer/plugins_update_check.lua
+++ skrypty/utils/installer/plugins_update_check.lua
@@ -5,15 +5,6 @@ scripts.plugins_update_check = scripts.plugins_update_check or {
 function scripts.plugins_update_check:github_check_version(plugin_name, repo)
     local cfg = {["plugin_name"] = plugin_name, ["repo"] = repo, ["storeKey"] = 'github_' .. repo .."_" .. plugin_name}
     cfg.url = "https://api.github.com/repos/" .. repo .. "/" .. plugin_name .. "/commits"
-    for i = 1, #self.plugins do
-        local config = self.plugins[i]
-        if config.url == cfg.url then
-            cfg.handlerId = registerAnonymousEventHandler("sysGetHttpDone", function(event, url, response) return self:github_on_GetHttpDone(url, response) end)
-            getHTTP(cfg.url)
-            return
-        end
-    end
-
     cfg.handlerId = registerAnonymousEventHandler("sysGetHttpDone", function(event, url, response) return self:github_on_GetHttpDone(url, response) end)
     table.insert(self.plugins, cfg)
     getHTTP(cfg.url)

39bab40ce9939e79fdab173867c9f78cbcce203e
Rawisz <r.wiszowaty@gmail.com>
2022-11-30 14:16:06 +0100

    Rozdzielenie tagu [par]: [par], [tar], [zbr]

--- skrypty/ui/gags/color/color_innych_parowanie.lua
+++ skrypty/ui/gags/color/color_innych_parowanie.lua
@@ -2,11 +2,15 @@ function trigger_func_skrypty_ui_gags_color_color_innych_parowanie_ktos_paruje()
     scripts.gags:gag_prefix("par", "innych_parowanie")
 end
 
+function trigger_func_skrypty_ui_gags_color_color_innych_parowanie_ktos_tarcza_paruje()
+    scripts.gags:gag_prefix("tar", "innych_parowanie")
+end
+
 function trigger_func_skrypty_ui_gags_color_color_innych_parowanie_ktos_zbroja_paruje()
     if matches[2] == "cie" then
         return
     end
 
-    scripts.gags:gag_prefix("par", "innych_parowanie")
+    scripts.gags:gag_prefix("zbr", "innych_parowanie")
 end
 

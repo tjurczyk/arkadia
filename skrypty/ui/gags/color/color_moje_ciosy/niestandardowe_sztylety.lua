1378d0ff253329399e9f1a984e9a21c33096b0d0
Rawisz <r.wiszowaty@gmail.com>
2022-12-15 21:22:40 +0100

    antyczny sztylet

--- skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_sztylety.lua
+++ skrypty/ui/gags/color/color_moje_ciosy/niestandardowe_sztylety.lua
@@ -1,3 +1,23 @@
+-- Antyczny sztylet
+
+function trigger_func_skrypty_ui_gags_ciosy_antyczny_sztylet()
+    local target = "moje_ciosy"
+    if matches["attacker"] then
+        target = matches["target"] == "cie" and "innych_ciosy_we_mnie" or "innych_ciosy"
+    end
+
+    local dmg = matches["damage"]
+    local value = -1
+        if dmg == "podluzna, niezbyt szeroka bruzde" then value = 1
+    elseif dmg == "podluzna, dosyc szeroka bruzde" then value = 2
+    elseif dmg == "poszarpana, dosyc szeroka bruzde" then value = 3
+    elseif dmg == "poszarpana, bardzo gleboka bruzde" then value = 4
+    elseif dmg == "potwornie poszarpana, gleboka bruzde" then value = 5
+    elseif dmg == "potwornie poszarpana, bardzo gleboka bruzde" then value = 6
+    end
+    scripts.gags:gag(value, 6, target)
+end
+
 -- Czarny smukly kordelas
 
 function trigger_func_skrypty_ui_gags_ciosy_czarny_smukly_kordelas()

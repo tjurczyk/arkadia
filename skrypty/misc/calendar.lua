494be4cc20ae90bc3089038a4e82742a193c208d
unknown <axesider@gmail.com>
2023-07-14 19:41:55 +0200

    Poprawki kalendarza Imperium

--- skrypty/misc/calendar.lua
+++ skrypty/misc/calendar.lua
@@ -23,6 +23,7 @@ misc["imperium_calendar"] = {
     ["Ulrichszeit"] = "Ulrichszeit (zima)",
     ["Vorhexen"] = "Vorhexen (pozna zima)",
     ["Hexentag"] = "Hexentag (Nowy Rok)",
+    ["Hexensnacht"] = "Hexensnacht (Nowy Rok)",
     ["Mitterfruhl"] = "Mitterfruhl (rownonoc wiosenna)",
     ["Sonnenstill"] = "Sonnenstill (przesilenie letnie)",
     ["Geheimnisnacht"] = "Geheimnisnacht (Noc Tajemnicy)",
@@ -30,22 +31,19 @@ misc["imperium_calendar"] = {
     ["Mondstill"] = "Mondstill (przesilenie zimowe)"
 }
 
-function misc:replace_string_calendar_ishtar(str)
-    local new_calendar_str = misc.ishtar_calendar[str]
-    if new_calendar_str and selectString(str, 1) > -1 then
-        replace(new_calendar_str)
+function misc:replace_string_calendar(str, calendar)
+    if calendar[str] == nil then
+        debugc("Brak wpisu '"..str.."' w kalendrzu")
+        return
     end
-end
-
-function misc:replace_string_calendar_imperium(str)
-    local new_calendar_str = misc.imperium_calendar[str]
+    local new_calendar_str = calendar[str]
     if selectString(str, 1) > -1 then
         replace(new_calendar_str)
     end
 end
 
 function trigger_func_skrypty_misc_calendar_pory_roku_ishtar()
-    misc:replace_string_calendar_ishtar(matches[2])
+    misc:replace_string_calendar(matches[2], misc["ishtar_calendar"])
 end
 
 function trigger_func_skrypty_misc_calendar_pory_roku_imperium()

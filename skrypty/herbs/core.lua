eb715fa861bd2a9a34b18cbc760d33d1840c6ec1
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2024-04-06 08:43:05 -0700

    fix herbs search when no db (#1542)
    Co-authored-by: Tomasz Jurczyk <tjurczyk@apple.com>

--- skrypty/herbs/core.lua
+++ skrypty/herbs/core.lua
@@ -14,7 +14,6 @@ function herbs:check_single_bag(herbs_str)
     if table.size(split_by_i) == 1 then
         -- this is single herb
         table.insert(items, split_by_i[1])
-
     elseif table.size(split_by_i) == 2 then
         -- more than one type in the bag
         table.insert(items, split_by_i[2])
@@ -48,19 +47,21 @@ function herbs:extract_herbs(herbs_arr)
         local int_number = string.match(split_by_words[1], "[0-9]+")
         if int_number then
             -- this is when "25 zoltych jasnych kwiatow"
-            item["name"] = herbs.herbs_long_to_short[table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
+            item["name"] = herbs.herbs_long_to_short
+                [table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
             item["amount"] = tonumber(int_number)
         else
-
             local liczebnik_number = scripts.string_to_liczebnik[split_by_words[1]]
             if liczebnik_number then
                 -- this is when "siedemnascie zoltych jasnych kwiatow"
-                item["name"] = herbs.herbs_long_to_short[table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
+                item["name"] = herbs.herbs_long_to_short
+                    [table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
                 item["amount"] = liczebnik_number
             else
                 if split_by_words[1] == "wiele" then
                     -- this is when "wiele zoltych jasnych kwiatow"
-                    item["name"] = herbs.herbs_long_to_short[table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
+                    item["name"] = herbs.herbs_long_to_short
+                        [table.concat(scripts:get_table_without_first_item(split_by_words), " ")]
                     item["amount"] = herbs["many_to_int"]
                 else
                     -- this is when "zolty jasny kwiat"
@@ -83,7 +84,7 @@ function herbs:search_herbs(search_phrase)
     cecho("\n <tomato>Znalazlem nastepujace ziola: \n\n")
     for herb_name, dict in pairs(herbs.herbs_details) do
         if string.match(dict["desc"], ".*" .. search_phrase .. ".*") or string.match(dict["details"], ".*" .. search_phrase .. ".*") or string.match(herb_name, ".*" .. search_phrase .. ".*") then
-            if herbs.index[herb_name] then
+            if herbs.index and herbs.index[herb_name] then
                 cecho("<grey> (<green>++<grey>) ")
             else
                 cecho("<grey> (--<grey>) ")
@@ -94,12 +95,14 @@ function herbs:search_herbs(search_phrase)
 end
 
 function herbs:get_printable_herb_dict(herb_name, herb_dict)
-    return "<light_slate_blue>" .. herb_dict["desc"] .. "<grey> (<yellow>" .. herb_name .. "<grey>) -> " .. herb_dict["details"]
+    return "<light_slate_blue>" ..
+        herb_dict["desc"] .. "<grey> (<yellow>" .. herb_name .. "<grey>) -> " .. herb_dict["details"]
 end
 
 function herbs:create_triggers()
     for herb_long, herb_name in pairs(herbs.herbs_long_to_short) do
-        scripts.tokens:register(herb_long, function(current_match) herbs:process_trigger(current_match, herb_name) end, "herbs " .. herb_name)
+        scripts.tokens:register(herb_long, function(current_match) herbs:process_trigger(current_match, herb_name) end,
+            "herbs " .. herb_name)
     end
 end
 
@@ -110,16 +113,17 @@ function herbs:process_trigger(herb_match, herb_name)
     if selectString(herb_match, 1) > -1 then
         replace(herb_match .. " (" .. herb_name .. ")")
     end
-    if selectString(herb_name, 1) > - 1 then
+    if selectString(herb_name, 1) > -1 then
         fg("blanched_almond")
     end
     local ln = getLineNumber()
     setLink(function()
         moveCursor(0, ln)
-            if selectString(herb_name, 1) > -1 then
-                creplace("<blanched_almond>" .. herb_name .. "<reset> " .. herbs.herbs_details[herb_name]["details"] .. "<reset>")
-            end
-            moveCursorEnd()
+        if selectString(herb_name, 1) > -1 then
+            creplace("<blanched_almond>" ..
+                herb_name .. "<reset> " .. herbs.herbs_details[herb_name]["details"] .. "<reset>")
+        end
+        moveCursorEnd()
     end, herbs.herbs_details[herb_name]["details"])
     resetFormat()
 end
@@ -131,7 +135,8 @@ function herbs:print_db(ver)
 
     cecho("\n")
     cecho("--------------------------- <green>Ziola w twojej bazie<grey> -------------------------------")
-    cecho("\n  <light_slate_blue>ile <grey>|        <light_slate_blue>nazwa <grey>            | <light_slate_blue>             dzialanie <grey>                      ")
+    cecho(
+        "\n  <light_slate_blue>ile <grey>|        <light_slate_blue>nazwa <grey>            | <light_slate_blue>             dzialanie <grey>                      ")
     cecho("\n--------------------------------------------------------------------------------")
 
     for k, v in pairs(herbs.index) do
@@ -142,7 +147,10 @@ function herbs:print_db(ver)
             end
             local amount_tmp = "    " .. tostring(amount)
             local name_str = string.sub(k .. "                      ", 0, 24)
-            local usage_str = string.sub(herbs.herbs_details[k]["details"] .. "                                                                 ", 0, 64)
+            local usage_str = string.sub(
+                herbs.herbs_details[k]["details"] .. "                                                                 ",
+                0,
+                64)
             cecho("\n<grey>  " .. string.sub(amount_tmp, #amount_tmp - 2, #amount_tmp) .. " | ")
             if ver and ver == "2" then
                 cecho(name_str)
@@ -163,7 +171,8 @@ function herbs:print_single(herbs_arr)
 
     cecho("\n")
     cecho("--------------------------- <green>Ziola w tym woreczku<grey> -------------------------------")
-    cecho("\n  <light_slate_blue>ile <grey>|        <light_slate_blue>nazwa <grey>            | <light_slate_blue>             dzialanie <grey>                      ")
+    cecho(
+        "\n  <light_slate_blue>ile <grey>|        <light_slate_blue>nazwa <grey>            | <light_slate_blue>             dzialanie <grey>                      ")
     cecho("\n--------------------------------------------------------------------------------")
 
     --cecho(herbs:get_printable_bag(herbs_arr))
@@ -185,7 +194,10 @@ function herbs:get_printable_bag(herbs_arr)
         end
 
         local name_str = string.sub(v["name"] .. "                      ", 0, 24)
-        local usage_str = string.sub(herbs.herbs_details[v["name"]]["details"] .. "                                                                 ", 0, 64)
+        local usage_str = string.sub(
+            herbs.herbs_details[v["name"]]["details"] ..
+            "                                                                 ",
+            0, 64)
 
         ret_str = ret_str .. "\n<grey>  " .. amount_str .. " | " .. name_str .. " | " .. usage_str
     end
@@ -194,4 +206,3 @@ function herbs:get_printable_bag(herbs_arr)
 end
 
 tempTimer(0.652, function() herbs:init_herbs() end)
-

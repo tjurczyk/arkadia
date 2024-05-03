96d6487fad78d3c58fddcd88e420085bf917ae0e
Tomasz Jurczyk <tjurczyk@apple.com>
2024-05-02 23:27:26 -0700

    add static button style

--- ui/toggle_button.lua
+++ ui/toggle_button.lua
@@ -18,7 +18,8 @@ function scripts.ui.toggle_button:onRelease()
 end
 
 function scripts.ui.toggle_button:updateStyleSheet()
-    local color = self.toggled and string.format("rgb(%s, %s, %s, %%s)", self.r, self.g, self.b) or "rgb(130, 30, 30, %s)"
+    local color = self.toggled and string.format("rgb(%s, %s, %s, %%s)", self.r, self.g, self.b) or
+        string.format("rgb(%s, %s, %s, %%s)", self.r_off, self.g_off, self.b_off)
     setLabelStyleSheet(self.id, scripts.ui.current_theme:get_button_stylesheet(color, self.font_size, self.size))
 end
 

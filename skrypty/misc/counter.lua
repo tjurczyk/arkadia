28ac03d4b5b4659a150333f0cdc035e94483cb06
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2024-07-08 09:25:27 -0700

    one more fix for db refactor (#1601)

--- skrypty/misc/counter.lua
+++ skrypty/misc/counter.lua
@@ -19,8 +19,8 @@ misc.counter2["db_log"] = db:create("countertwolog", {
     }
 })
 
-misc.counter2["db_daysum"] = db:create("countertwo_daysum", {
-    countertwo_daysum = {
+misc.counter2["db_daysum"] = db:create("countertwodaysum", {
+    countertwodaysum = {
         year = 0,
         month = 0,
         day = 0,
